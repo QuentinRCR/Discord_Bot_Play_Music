40b3e6bb2cdd5a0123f8e784d1a04e38ab9735ed
Quentin REY <quentin.rey@etu.emse.fr>
2023-03-04 17:16:20 +0100

    fix but with unwanted responses what /stop and /resume

--- lib/Main.rb
+++ lib/Main.rb
@@ -2,6 +2,7 @@ require 'discordrb'
 require_relative 'song'
 require_relative 'bot'
 require_relative 'bot_credentials'
+require_relative 'Music_queue'
 
 class Main
 
@@ -16,13 +17,24 @@ class Main
 
     #when the player executes the command /music
     bot.command :play do |event|
-      url = event.message.content.gsub(prefix+"play", '') # Extract the URL from the message content
-      song=Song.new(url)
-      song.download_song #download the sound
+      #create the queue that handles the flow
+      #queue = MusicQueue.new()
+      #puts queue.display
+
+      # Extract the URL from the message content
+      url = event.message.content.gsub(prefix+"play", '')
+
+      # add the sound to the queue
+      # queue.push(Song.new(url))
+      song = Song.new(url)
+      song.download_song
+
       voice_bot = bot.connect_user_voice_chanel(event) #Connect to the user channel
       if voice_bot!=nil #if the player was connected to a voice channel
         song.play(voice_bot,event) # Play the song
+        #queue.pop
       end
+
     end
 
     bot.command :quit do |event|
@@ -31,10 +43,12 @@ class Main
 
     bot.command :stop do |event|
       bot.pause_voice(event)
+      return nil #to avoid unwanted responses in the chat
     end
     #
     bot.command :resume do |event|
       bot.resume_voice(event)
+      return nil #to avoid unwanted responses in the chat
     end
 
     bot.run(false) #run the bot forever
